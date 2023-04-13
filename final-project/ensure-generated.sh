24b260d761433501c66306243d7e34bd9c6e0ba7
Carson McManus <carson.mcmanus1@gmail.com>
2023-04-13 12:54:49 -0400

    loosen ci check

--- final-project/ensure-generated.sh
+++ final-project/ensure-generated.sh
@@ -8,8 +8,15 @@ cd "$(dirname "$0")"
 
 ./render-figures.sh
 
-if ! git diff --quiet; then
+if [[ "$(git ls-files -o --directory --exclude-standard)" != "" ]]; then
   echo "Generated files are out of date. Please run './render-figures.sh' and commit the changes."
-  git diff
+  git ls-files -o --directory --exclude-standard
   exit 1
 fi
+
+# mermaid cli is buggy and doesn't render the same thing every time
+# if ! git diff --quiet; then
+#   echo "Generated files are out of date. Please run './render-figures.sh' and commit the changes."
+#   git diff
+#   exit 1
+# fi
